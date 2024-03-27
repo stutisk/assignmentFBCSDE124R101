3204e684329d293e337963b74d9c00ec2708b394
stutisk <48529714+stutisk@users.noreply.github.com>
2024-03-28 04:45:44 +0530

    speedometer

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import { FilterPage } from "./Pages/FilterPage";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FilterPage value={75} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
